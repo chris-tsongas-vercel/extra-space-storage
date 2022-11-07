2eecd0c063a40beb9cbd1ce6e7f30e3dd5936504
Chris Tsongas <chris.tsongas@vercel.com>
2022-11-07 12:45:53 -0800

    add redirct

--- apps/redirect/next.config.js
+++ apps/redirect/next.config.js
@@ -2,6 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  async redirects() {
+    return [
+      {
+        source: "/google",
+        destination: "https://www.google.com",
+        permanent: false,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
